43223af5bc7e7eb451ff657f5ebe3c1ba954e190
RobertJN64 <robertjnies@gmail.com>
2025-03-26 02:35:37 -0400

    some basic ll functions

--- linked_list.c
+++ linked_list.c
@@ -2,13 +2,32 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) {
+  struct list_node *new_node = malloc(sizeof(struct list_node));
+  if (new_node == NULL) {
+    return NULL;
+  }
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+  new_node->value = value;
+  return new_node;
+}
+
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *old_head = list->head;
+  list->head = new_node(value);
+  list->head->next = old_head;
+}
 
 void insert_at_tail(struct linked_list *list, size_t value) {}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) {
+  struct list_node *old_head = list->head;
+  list->head = old_head->next;
+
+  size_t value = old_head->value;
+  free(old_head);
+  return value;
+}
 
 size_t remove_from_tail(struct linked_list *list) { return 0; }
 
